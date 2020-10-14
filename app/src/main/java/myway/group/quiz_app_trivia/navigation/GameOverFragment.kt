625cfa4801fc654712ba9c6c4f9027d855241e83
Admin <arab1997>
2020-10-14 18:29:18 +0500

    third lesson ex:24

--- app/src/main/java/myway/group/quiz_app_trivia/navigation/GameOverFragment.kt
+++ app/src/main/java/myway/group/quiz_app_trivia/navigation/GameOverFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import myway.group.quiz_app_trivia.R
 import myway.group.quiz_app_trivia.databinding.FragmentGameOverBinding
 
@@ -13,7 +14,10 @@ class GameOverFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_over, container, false)
+            inflater, R.layout.fragment_game_over, container, false)
+        binding.tryAgainButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
+        }
         return binding.root
     }
 }
--- app/src/main/java/myway/group/quiz_app_trivia/navigation/MainActivity.kt
+++ app/src/main/java/myway/group/quiz_app_trivia/navigation/MainActivity.kt
@@ -3,6 +3,8 @@ package myway.group.quiz_app_trivia.navigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import myway.group.quiz_app_trivia.R
 import myway.group.quiz_app_trivia.databinding.ActivityMainBinding
 
@@ -12,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
